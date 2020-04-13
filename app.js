0c140007022f355fcb185ea87a9b1a8cc2104d29
Amiinoz <momagan@gmail.com>
2020-04-13 16:46:58 -0600

    Reading user input data

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
 "usestrict";
-// Budget controller
+// BUDGET CONTROLLER
 let budgetController = (function () {
   //code
 })();
 
-// Ui controller
+// UI CONTROLLER
 let uiController = (function () {
-  //code
+  let DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn",
+  };
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+    // exposing the DOMstrings globaly
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
 })();
 
-// Global app controller
+// GOLOBAL APP CONTROLLER
 let controller = (function (budgetCtrl, UICtrl) {
+  let DOM = UICtrl.getDOMstrings();
+
   let ctrlAddItem = function () {
     // 1. Get the field input data
+    let input = UICtrl.getInput();
+    console.log(input);
+
     // 2. Add the item to the budget controller
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
-    console.log("It works");
   };
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
   document.addEventListener("keypress", function (event) {
     if (event.keyCode === 13 || event.which === 13) {

9c848790b1477ef91be599921fb2624998f256d4
Amiinoz <momagan@gmail.com>
2020-04-13 16:09:52 -0600

    Setting first event listeners

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 "usestrict";
+// Budget controller
 let budgetController = (function () {
-  let x = 23;
-
-  let add = function (a) {
-    return x + a;
-  };
-  return {
-    puplicTest: function (b) {
-      console.log(add(b));
-    },
-  };
-})();
-
-var uiController = (function () {
   //code
 })();
 
-var controller = (function (budgetCtrl, UICtrl) {})(
-  budgetController,
-  uiController
-);
+// Ui controller
+let uiController = (function () {
+  //code
+})();
+
+// Global app controller
+let controller = (function (budgetCtrl, UICtrl) {
+  let ctrlAddItem = function () {
+    // 1. Get the field input data
+    // 2. Add the item to the budget controller
+    // 3. Add the item to the UI
+    // 4. Calculate the budget
+    // 5. Display the budget on the UI
+    console.log("It works");
+  };
+  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+
+  document.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
+})(budgetController, uiController);
